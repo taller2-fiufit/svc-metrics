98d57e8999a845850c710e0ca80decad5c044cb5
Federico Colangelo <fcolangelo@fi.uba.ar>
2023-04-27 13:14:41 -0300

    Configuracion CI/CD terminada

--- src/consumer/messageHandler.ts
+++ src/consumer/messageHandler.ts
@@ -4,13 +4,28 @@ import * as AWS from 'aws-sdk';
 import { MetricsService } from '../metrics/metrics.service';
 import { CreateMetricDto } from '../metrics/dtos/create-metric.dto';
 import { plainToClass } from 'class-transformer';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+
+config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+const configService = new ConfigService();
+const queueName = configService.get<string>('QUEUE_NAME');
 
 @Injectable()
 export class MessageHandler {
     constructor(private metricsService: MetricsService) {}
-    @SqsMessageHandler('MetricsQueue_Development', false)
+    @SqsMessageHandler(queueName, false)
     async handleMessage(message: AWS.SQS.Message) {
-        const metric = plainToClass(CreateMetricDto, JSON.parse(message.Body));
+        let rawMessage = JSON.parse(message.Body);
+        const metric = this.parseMetric(rawMessage)
         this.metricsService.create(metric.service, metric.command, metric.timeStamp);
+    }
+
+    private parseMetric(message: any): CreateMetricDto {
+      message.timeStamp = new Date(message.timeStamp).toISOString();
+      return plainToClass(CreateMetricDto, message);
     }
 }
--- src/migrations/1682606563487-initial-schema.ts
+++ src/migrations/1682606563487-initial-schema.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class InitialSchema1682558836852 implements MigrationInterface {
-    name = 'InitialSchema1682558836852'
+export class InitialSchema1682606563487 implements MigrationInterface {
+    name = 'InitialSchema1682606563487'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "metric" ("id" SERIAL NOT NULL, "timeStamp" TIMESTAMP NOT NULL, "service" character varying NOT NULL, "command" character varying NOT NULL, CONSTRAINT "PK_7d24c075ea2926dd32bd1c534ce" PRIMARY KEY ("id"))`);
