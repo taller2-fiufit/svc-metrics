19c809a2cdd9f1e4c17baabf54ccde0374e4d6c1
Tomas Gruner <47506558+MegaRedHand@users.noreply.github.com>
2023-05-21 21:20:14 -0300

    Add test for training event

--- test/metrics.e2e-spec.ts
+++ test/metrics.e2e-spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from '../src/app.module';
-import { Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
+
+import * as request from 'supertest';
+import { Repository } from 'typeorm';
+
 import { Metric } from '../src/metrics/metrics.entity';
+import { AppModule } from '../src/app.module';
 import { UsersMetricsDto } from '../src/metrics/dtos/users-metrics.dto';
+import { CreateMetricDto } from '../src/metrics/dtos/create-metric.dto';
 import { TrainingsMetricsDto } from '../src/metrics/dtos/trainings-metrics.dto';
 
 describe('Sistema de Métricas', () => {
@@ -43,6 +46,26 @@ describe('Sistema de Métricas', () => {
         const trainingsMetrics: TrainingsMetricsDto = res.body;
         expect(trainingsMetrics.trainingsCreated).toEqual(0);
         expect(trainingsMetrics.trainingsFavorited).toEqual(0);
+      });
+  });
+
+  it('las métricas inician en 0', async () => {
+    const metric = metricsRepository.create({
+      timestamp: new Date().toISOString(),
+      service: 'trainings',
+      command: 'trainingCreated',
+      attrs: '{}',
+    });
+
+    await request(app.getHttpServer())
+      .get(`/metrics/trainings/events/${metric.command}`)
+      .query({ from: metric.timestamp, to: metric.timestamp })
+      .expect(200)
+      .then((res) => {
+        const trainingsMetrics: CreateMetricDto[] = res.body;
+        expect(trainingsMetrics.length).toEqual(1);
+        const expectedMetric = { attrs: JSON.parse(metric.attrs), ...metric };
+        expect(trainingsMetrics).toEqual([expectedMetric]);
       });
   });
 });
